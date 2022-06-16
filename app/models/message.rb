439be74c01ad1c01cb0859a4d94338d7fd343070
antopalidi <t.opalidisanna@gmail.com>
2022-06-16 17:12:53 +0200

    add like model

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,8 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :room
 
+  has_many :likes
+
   scope :sorted, -> { order(:id) }
 
   validates :body, presence: true, max_length: 255
