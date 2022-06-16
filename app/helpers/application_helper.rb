ef3656b7398e7546afabc72f8349c46c6b064d84
antopalidi <t.opalidisanna@gmail.com>
2022-06-16 13:15:37 +0200

    add devise

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def class_for_flash(key)
+    {
+      alert: 'alert-danger',
+      notice: 'alert-success'
+    }.stringify_keys[key.to_s]
+  end
+
+  def flash_message
+    flash.map do |key, message|
+      concat(content_tag(:div, message, class: "alert #{class_for_flash(key)} alert-dismissible fade show mt-4", role: 'alert') do
+        concat content_tag(:button, '',type: 'button', class: 'btn-close', data: { bs_dismiss: 'alert' })
+        concat message
+      end)
+    end
+    nil
+  end
 end
